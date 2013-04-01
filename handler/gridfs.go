4b1a99a58f1ae9907b436693d7063084b3ae9c71
Nassor Paulino da Silva <nassor@inovati.inf.br>
2013-04-01 11:01:55 -0300

    Disjunct functions in folders

---
+++ handler/gridfs.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+	"io"
+
+	. "kuenea/conf"
+	"labix.org/v2/mgo"
+)
+
+type gridFSHandler struct {
+	GFS  *mgo.GridFS
+	Path string
+}
+
+func (g *gridFSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	file, err := g.GFS.Open(strings.Replace(r.URL.Path[1:], g.Path, "", 1))
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", file.ContentType())
+	file.Close()
+
+}
+
+// Handle server requests, find file and response.
+func GridFSServer(gfs *mgo.GridFS, config *Config) http.Handler {
+	return &gridFSHandler{gfs, config.Database.Path}
+}
+
--- kuenea.go
+++ kuenea.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"strings"
 	"time"
-
+	
+	. "kuenea/conf"
+	"kuenea/handler"
 	"labix.org/v2/mgo"
 )
 
@@ -28,7 +27,7 @@ func main() {
 	session := mdbSession.DB(config.Database.DBName)
 	gfs = session.GridFS("fs")
 
-	http.HandleFunc("/"+config.Database.Path, gridHandler)
+	http.Handle("/"+config.Database.Path, handler.GridFSServer(gfs, &config))
 	s := &http.Server{
 		Addr:         config.BindWithPort(),
 		ReadTimeout:  time.Duration(config.Http.Timeout) * time.Millisecond,
@@ -37,21 +36,3 @@ func main() {
 	s.ListenAndServe()
 }
 
-// Handle server requests, find file and response.
-func gridHandler(w http.ResponseWriter, r *http.Request) {
-	file, err := gfs.Open(strings.Replace(r.URL.Path[1:], config.Database.Path, "", 1))
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "%s", "Not Found")
-		return
-	}
-	
-	w.Header().Set("Content-Type", file.ContentType())
-	_, err = io.Copy(w, file)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "%s", "Can't reach file")
-	}
-	
-	file.Close()
-}
